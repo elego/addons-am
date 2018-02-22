189ff8d9abe2df50f1cf4cf8bb6ed09e6f8839ac
aek <aekroft@gmail.com>
2018-02-22 03:07:36 -0500

    add parent form onchange for o2m on create

--- fix_o2m_onchange/static/src/js/widget.js
+++ fix_o2m_onchange/static/src/js/widget.js
@@ -2,6 +2,28 @@ openerp.fix_o2m_onchange = function(instance) {
 
     instance.web.form.One2ManyListView.include({
         changed_records: function () {}
-    })
+    });
+
+    instance.web.BufferedDataSet.include({
+        create: function(data, options) {
+            var res = this._super.apply(this, arguments);
+            // this.trigger('dataset_changed', data, options);
+            this.o2m.trigger('created_value');
+            return res;
+        }
+    });
+
+    instance.web.FormView.include({
+        register_field: function (field, name) {
+            this._super.apply(this, arguments);
+            field.on('created_value', this, function() {
+                this.trigger('field_changed:' + name);
+                field._dirty_flag = true;
+                this.do_onchange(field);
+                this.on_form_changed(true);
+                this.do_notify_change();
+            });
+        }
+    });
 
 };
